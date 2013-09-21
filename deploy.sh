fb66071e3e2fe61fd1aadc337d9e8bb8eb474513
Liauw Fendy <lfendy@gmail.com>
2013-09-21 22:45:28 +1000

    more automation on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+git submodule init
+git submodule update
+
+vim +BundleInstall +qall
+
 if [ -e $HOME/.vim ]; then mv $HOME/.vim{,_bak}; fi
 if [ -e $HOME/.vimrc ]; then mv $HOME/.vimrc{,_bak}; fi
 
