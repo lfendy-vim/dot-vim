41c0f2dabc33b27f489bdf3c0bcc9581104b6fdb
Liauw Fendy <lfendy@gmail.com>
2015-08-02 16:33:06 +1000

    added tmux conf

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 git submodule init
 git submodule update
 
-vim +BundleInstall +qall
+vim +PluginInstall +qall
 
 if [ -e $HOME/.vim ]; then mv $HOME/.vim{,_bak}; fi
 if [ -e $HOME/.vimrc ]; then mv $HOME/.vimrc{,_bak}; fi
+if [ -e $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf{,_bak}; fi
 
 ln -s `pwd` $HOME/.vim
 ln -s `pwd`/.vimrc $HOME/.vimrc
+ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
