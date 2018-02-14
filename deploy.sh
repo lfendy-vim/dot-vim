b7089fdeaa5df5d1692550e2bb8b84f8131c6bd0
EC2 Default User <ec2-user@cbdcbbo765905d.cbdc.extnp.national.com.au>
2018-02-15 09:56:56 +1100

    change order of deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 git submodule init
 git submodule update
 
-vim +PluginInstall +qall
-
 if [ -e $HOME/.vim ]; then mv $HOME/.vim{,_bak}; fi
 if [ -e $HOME/.vimrc ]; then mv $HOME/.vimrc{,_bak}; fi
 if [ -e $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf{,_bak}; fi
@@ -11,3 +9,6 @@ if [ -e $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf{,_bak}; fi
 ln -s `pwd` $HOME/.vim
 ln -s `pwd`/.vimrc $HOME/.vimrc
 ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
+
+vim +PluginInstall +qall
+
